a26a11137b59804931100f4cd751967ae5449069
Zxade <zxadeel@hotmail.com>
2023-10-06 23:03:00 -0400

    Created Stack Navigator

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator} from '@react-navigation/stack';
+import TimerScreen from './src/screens/TimerScreen';
+
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on yourzxcvzxvxvxzvzxv apkadsnflkajsdlkfjaslp!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen name="TimerScreen" component={TimerScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
 
