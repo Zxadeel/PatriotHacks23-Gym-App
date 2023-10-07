3f736faf8c1ce5ec0bcb152e6d933d8d903ed941
Zxade <zxadeel@hotmail.com>
2023-10-07 13:56:21 -0400

    made a screen

---
+++ src/components/Button.js
@@ -0,0 +1,18 @@
+import { Touchable, TouchableOpacity, View } from "react-native";
+
+export default function BeginExpScreen() {
+
+    return (
+        <View>
+            <TouchableOpacity>
+                <Text>Random Text</Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+styles = StyleSheet.create({
+    btn:{
+
+    }
+})
---
+++ src/screens/BeginExpScreen.js
@@ -0,0 +1,10 @@
+import { Touchable, TouchableOpacity, View } from "react-native";
+
+export default function BeginExpScreen() {
+
+    return (
+        <View>
+            
+        </View>
+    )
+}
