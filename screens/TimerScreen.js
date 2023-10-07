bc7efb96c8b5661701b55de33673afe3aa15197e
porofesorsad <porofesorsad@gmail.com>
2023-10-06 22:41:27 -0400

    COOL NEW TIMER SCREEN

---
+++ screens/TimerScreen.js
@@ -0,0 +1,49 @@
+// Import necessary modules from React and React Native
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+// Define a TimerScreen component
+const TimerScreen = () => {
+  // Define a state variable called "seconds" and a function to update it called "setSeconds"
+  const [seconds, setSeconds] = useState(0);
+
+  // Use the useEffect hook to set up an interval that increments "seconds" every second
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds(seconds => seconds + 1);
+    }, 1000);
+
+    // Return a cleanup function that clears the interval when the component unmounts
+    return () => clearInterval(interval);
+  }, []);
+
+  // Define a function called "formatTime" that formats the time as "mm:ss"
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+  };
+
+  // Render a View component with a Text component that displays the formatted time
+  return (
+    <View style={styles.container}>
+      <Text style={styles.timer}>{formatTime(seconds)}</Text>
+    </View>
+  );
+};
+
+// Define a styles object that contains styles for the TimerScreen component
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  timer: {
+    fontSize: 48,
+    fontWeight: 'bold',
+  },
+});
+
+// Export the TimerScreen component as the default export of this module
+export default TimerScreen;
