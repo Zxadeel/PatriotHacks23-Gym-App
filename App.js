47f7ff499a6829c14ffd4e272e97839773eb7a23
LyLy Tran <lylytr.311@gmail.com>
2023-10-07 21:12:36 -0400

    button images

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator} from '@react-navigation/stack';
 import TimerScreen from './src/screens/TimerScreen';
 import WelcomeScreen from './src/screens/WelcomeScreen';
+import DaysScreen from './src/screens/DaysScreen';
 
 
 const Stack = createStackNavigator(); //intialize screen navigator
@@ -13,6 +14,7 @@ export default function App() {
       <NavigationContainer>
         <Stack.Navigator screenOptions={{headerShown: false}}>
           {/* <Stack.Screen name="Welcome" component={WelcomeScreen} /> */}
+          <Stack.Screen name="How many days a week" component={DaysScreen} />
           <Stack.Screen name="Timer Screen" component={TimerScreen} />
         </Stack.Navigator>
       </NavigationContainer>
